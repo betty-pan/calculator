a51254d3d97ada80a43598e8d7f8b7fa0a414321
BettyPan <betty19931218@gmail.com>
2020-12-25 13:04:51 +0800

    First Edition

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var num1TextField: UITextField!
+    @IBOutlet weak var num2TextField: UITextField!
+    @IBOutlet weak var sumLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func calculate(_ sender: UISegmentedControl) {
+        
+        let num1 = Float(num1TextField.text!)
+        let num2 = Float(num2TextField.text!)
 
-
+        
+        if sender.selectedSegmentIndex == 0 {
+            sumLabel.text = "\(num1! + num2!)"
+        }else if sender.selectedSegmentIndex == 1 {
+            sumLabel.text = "\(num1! - num2!)"
+        }else if sender.selectedSegmentIndex == 2 {
+            sumLabel.text = "\(num1! * num2!)"
+        }else{
+            sumLabel.text = String(format: "%.2f", num1!/num2!)
+            
+        }
+    }
 }
 
